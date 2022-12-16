93e593a299ecaa8b8cec4a038b1580b5161d6522
TropicSapling <tropical.productions.scratch@gmail.com>
2022-12-16 01:23:16 +0100

    Now??

--- frontend/index.js
+++ frontend/index.js
@@ -8,7 +8,7 @@ window.onload = function init() {
 	canvas = document.getElementById("game").getContext("2d");
 }
 
-window.draw_bg = export function draw_bg() {
+window.draw_bg = function draw_bg() {
 	// Clear canvas
 	canvas.clearRect(0, 0, 600, 600);
 	
@@ -17,7 +17,7 @@ window.draw_bg = export function draw_bg() {
 	canvas.fillRect(0, 0, 600, 600);
 }
 
-window.draw_agent = export function draw_agent(r, g, b, x, y, size) {
+window.draw_agent = function draw_agent(r, g, b, x, y, size) {
 	canvas.fillStyle = `rgb(r, g, b)`;
 	canvas.fillRect(x, y, size, size);
 }
