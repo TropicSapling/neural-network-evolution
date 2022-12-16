b11d5c183269f60a0df65c9d28294e4979af9c22
TropicSapling <tropical.productions.scratch@gmail.com>
2022-12-16 20:09:34 +0100

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -5,6 +5,11 @@ import init, {run} from '../wasm/neural-network-evolution.js'
 const FPS = 60;
 
 let canvas;
+let loop;
+
+window.stopAll = function stopAll() {
+	clearInterval(loop);
+}
 
 window.onload = function init() {
 	canvas = document.getElementById("game").getContext("2d");
@@ -25,7 +30,7 @@ window.draw_agent = function draw_agent(r, g, b, x, y, size) {
 }
 
 init().then(() => {
-	console.log("Finished loading WebAssembly.")
-	console.log(`Running game at ${FPS} FPS.`)
-	setInterval(run, 1000/FPS)
+	console.log("Finished loading WebAssembly.");
+	console.log(`Running game at ${FPS} FPS.`);
+	loop = setInterval(run, 1000/FPS);
 })
