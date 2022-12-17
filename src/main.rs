c6aab4e50800f904783cbabf53cbc8d9320ad4e6
TropicSapling <tropical.productions.scratch@gmail.com>
2022-12-17 19:52:37 +0100

    More randomness

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,8 @@ mod structs;
 mod game;
 mod ai;
 
-use rand::Rng;
+use std::{ops::Range, cmp::PartialOrd};
+use rand::{Rng, distributions::uniform::SampleUniform};
 use wasm_bindgen::prelude::*;
 
 use js::*;
@@ -23,17 +24,26 @@ fn main() {
 	println!("Don't run it this way; compile to wasm!")
 }
 
+fn rand_range<T: SampleUniform + PartialOrd>(range: Range<T>) -> T {
+	rand::thread_rng().gen_range(range)
+}
+
+fn rand_col() -> Colour {
+	Colour {r: rand_range(0..256), g: rand_range(0..256), b: rand_range(0..256)}
+}
+
+fn rand_pos() -> Pos {
+	Pos {x: rand_range(0.0..450.0), y: rand_range(0.0..450.0)}
+}
+
 #[wasm_bindgen(start)]
 pub unsafe fn start() {
-	let x = rand::thread_rng().gen_range(100.0..500.0);
-	let y = rand::thread_rng().gen_range(100.0..500.0);
-
 	// Just for testing
 	AGENTS.push(Agent {
 		neurons : vec![],
-		colour  : Colour {r: 67, g: 45, b: 123},
-		pos     : Pos    {x, y},
-		size    : 123,
+		colour  : rand_col(),
+		pos     : rand_pos(),
+		size    : rand_range(64..128),
 		angle   : 0.0,
 
 		moving  : true,
