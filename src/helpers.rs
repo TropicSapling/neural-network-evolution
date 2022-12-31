db8c5bcba509599c81336170aec98db3d10467dc
TropicSapling <tropical.productions.scratch@gmail.com>
2022-12-31 02:01:51 +0100

    Simplifications

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,6 +1,9 @@
-use std::{ops::RangeBounds, cmp::PartialOrd};
+use std::ops::RangeBounds;
 use rand::{Rng, distributions::uniform::{SampleRange, SampleUniform}};
 
-pub fn rand_range<T: SampleUniform + PartialOrd, R: RangeBounds<T> + SampleRange<T>>(range: R) -> T {
+pub fn rand_range<T, R>(range: R) -> T
+	where T: SampleUniform,
+	      R: RangeBounds<T> + SampleRange<T>
+{
 	rand::thread_rng().gen_range(range)
 }
