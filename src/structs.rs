f561b126c6e34329b171a26417dfe3c5ff7e80fd
TropicSapling <tropical.productions.scratch@gmail.com>
2023-02-19 22:49:58 +0100

    slightly better debug

--- src/structs.rs
+++ src/structs.rs
@@ -35,17 +35,17 @@ pub struct Neuron {
 impl fmt::Debug for Neuron {
 	// Print neuron debug info in a concise way
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    	let mut s = String::from("Neuron->[");
+    	let mut s = String::from("Neuron {");
 
     	let mut conn_iter = self.next_conn.iter().peekable();
     	while let Some(conn) = conn_iter.next() {
-    		s += &format!("{}*#{}", conn.weight, conn.dest_index);
+    		s += &format!("(*{})->#{}", conn.weight, conn.dest_index);
     		if !conn_iter.peek().is_none() {
     			s += ", "
     		}
     	}
 
-        write!(f, "{s}]")
+        write!(f, "{s}}}")
     }
 }
 
