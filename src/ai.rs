8f2d816c3a9285c1ccad1c82569715c7c455c52f
TropicSapling <tropical.productions.scratch@gmail.com>
2023-02-20 22:13:49 +0100

    Minor improvements

--- src/ai.rs
+++ src/ai.rs
@@ -1,4 +1,4 @@
-use crate::{Agent, js::*};
+use crate::Agent;
 
 pub fn update_ai(agents: &mut Vec<Agent>) {
 	for agent in agents {
@@ -8,16 +8,16 @@ pub fn update_ai(agents: &mut Vec<Agent>) {
 		//let nearest = agent.get_nearest_of(&agents);
 		let nearest = Agent::new(&mut vec![]); // dummy for now
 
-		input[0].excitation = 1;
-		input[1].excitation = 1;
+		// Input neurons always fire
+		(input[0].excitation, input[0].act_threshold) = (1, 1);
+		(input[1].excitation, input[1].act_threshold) = (1, 1);
 
 		for conn in &mut input[0].next_conn {
 			//conn.weight = agent.norm_dist_to(nearest_agent);
 		}
 
 		for conn in &mut input[1].next_conn {
-			conn.weight = (body.size/nearest.body.size * 100.0) as isize;
-			console_log!("size_diff={}", conn.weight); // debug
+			conn.weight = (body.size/nearest.body.size * 5.0) as isize;
 		}
 
 		let output = agent.brain.update_neurons();
