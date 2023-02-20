f3f0973c54d685079d54361b83df90f25cb82484
TropicSapling <tropical.productions.scratch@gmail.com>
2023-02-20 23:07:46 +0100

    ???

--- src/ai.rs
+++ src/ai.rs
@@ -22,10 +22,8 @@ pub fn update_ai(agents: &mut Vec<Agent>) {
 
 		// Debug
 		if rand_range(0..2048) == 0 {
-			if input[0].next_conn.len() > 0 && input[1].next_conn.len() > 0 {
-				console_log!("dist={}", (nearest.1/720_000.0 * 5.0));
-				console_log!("size_diff={}", (body.size/nearest.0 * 5.0));
-			}
+			console_log!("dist={}", (nearest.1/720_000.0 * 5.0));
+			console_log!("size_diff={}", (body.size/nearest.0 * 5.0));
 		}
 
 		let output = agent.brain.update_neurons();
--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ pub unsafe fn start() {
 		AGENTS.push(Agent::new(&mut AGENTS));
 	}
 
-	console_log!("Starting version 0.0.44");
+	console_log!("Starting version 0.0.45");
 }
 
 #[wasm_bindgen]
