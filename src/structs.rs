3f8b91e59e775ece5986e812a5063439087402ff
TropicSapling <tropical.productions.scratch@gmail.com>
2023-03-05 01:46:12 +0100

    Turning off STDP for now
    Is ineffective in its current form.

--- src/structs.rs
+++ src/structs.rs
@@ -283,7 +283,7 @@ impl Brain {
 					&mut self.neurons_hidden[conn.dest_index - OUTS]
 				};
 
-				let prev_recv_excitation = recv_neuron.excitation;
+				//let prev_recv_excitation = recv_neuron.excitation;
 
 				if conn.relu {
 					recv_neuron.excitation += conn.weight * excitation
@@ -292,7 +292,7 @@ impl Brain {
 
 					// STDP (Spike-Timing-Dependent Plasticity)
 					// TODO: maybe make more realistic?
-					if prev_recv_excitation >= recv_neuron.act_threshold {
+					/*if prev_recv_excitation >= recv_neuron.act_threshold {
 						// Receiver already has fired => weaken connection
 						if conn.weight.abs() > 1.0 {
 							Neuron::expand_or_shrink(&mut conn.weight, -1.0)
@@ -302,7 +302,7 @@ impl Brain {
 						if conn.weight.abs() < 8.0 {
 							Neuron::expand_or_shrink(&mut conn.weight, 1.0)
 						}
-					}
+					}*/
 				}
 
 				recv_neuron.reachable = true
