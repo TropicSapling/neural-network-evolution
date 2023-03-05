269166b6dad61b1d19e7464db7517eba4aa64be3
TropicSapling <tropical.productions.scratch@gmail.com>
2023-03-05 01:37:11 +0100

    Only STDP for non-ReLU

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ static mut AGENTS: Vec<Agent> = vec![];
 
 #[wasm_bindgen(start)]
 pub unsafe fn start() {
-	console_log!("Starting version 0.2.2")
+	console_log!("Starting version 0.2.3")
 }
 
 #[wasm_bindgen]
