b7528104bdef350ba396b4bec68e10183e7e3fb3
TropicSapling <tropical.productions.scratch@gmail.com>
2024-10-02 20:29:02 +0200

    Tweaks

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ static AGENTS: Mutex<Vec<Agent>> = Mutex::new(vec![]);
 
 #[wasm_bindgen(start)]
 pub fn start() {
-	console_log!("Starting version 0.2.7")
+	console_log!("Starting version 0.2.8")
 }
 
 #[wasm_bindgen]
